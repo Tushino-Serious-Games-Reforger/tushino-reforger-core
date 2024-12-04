efcca5c6f473a10b3310c270f6a28dd953272fa3
Boryan <tweedy.harfunkel@gmail.com>
2024-12-05 02:45:38 +0300

    Added mission time component

---
+++ Tushino_VanillaScenarios/Scripts/Game/PS_MissionTime.c
@@ -0,0 +1,43 @@
+class PS_MissionTimeClass: GenericEntityClass {
+
+}
+
+
+class PS_MissionTime: GenericEntity {
+    [Attribute(defvalue: "60", desc: "Mission time in minutes")]
+    int m_iMissionTimeMinutes;
+	[Attribute(defvalue: "10", desc: "Approximate resolution time, " +
+	          "mission will be advanced in general within " +
+	          "m_iMissionTimeMinutes + m_iMissionTimeResolutionDelaySeconds, " +
+	          "altorough there may be more delay if server is lagging")]
+	int m_iMissionTimeResolutionDelaySeconds;
+	float m_fMissionTimeMillis;
+	WorldTimestamp m_lastFreezeTimestamp;
+    PS_GameModeCoop m_game;	
+
+    override void EOnActivate(IEntity owner) {
+		if (Replication.IsServer()) {
+			m_fMissionTimeMillis = 60.0 * 1000 * m_iMissionTimeMinutes;
+			m_game = PS_GameModeCoop.Cast(GetGame().GetGameMode());
+			m_lastFreezeTimestamp = GetWorld().GetTimestamp();
+
+			int delay = m_iMissionTimeResolutionDelaySeconds * 1000;
+			GetGame().GetCallqueue().CallLater(fn: CheckTime, delay: delay, repeat: true);
+		}
+    }
+	
+	void CheckTime() {
+		SCR_EGameModeState gameState = m_game.GetState();
+		if ((gameState == SCR_EGameModeState.GAME)) {
+			if (m_game.IsFreezeTimeEnd()) {
+				WorldTimestamp currentTimestamp = GetWorld().GetTimestamp();
+				float elapsedTimeMillis = currentTimestamp.DiffMilliseconds(m_lastFreezeTimestamp);
+				if (elapsedTimeMillis > m_fMissionTimeMillis) {
+					m_game.AdvanceGameState(gameState);
+				}
+			} else {
+				m_lastFreezeTimestamp = GetWorld().GetTimestamp();
+			}
+		}
+	}
+}
