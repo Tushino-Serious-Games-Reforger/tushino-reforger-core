bd0993514a2fe3bfc1a25943a1ce88a1429c2135
twinkedmeadow <162931484+twinkedmeadow@users.noreply.github.com>
2024-12-14 20:46:06 +0300

    Сделал миссию отдаленно похожую на миссию Депутат (#15)
    * Hunt mission
    
    * Rebalance mission a bit
    
    * Add automatic objective completion for destruction/evacuation/elimination when possible in my missions
    
    * Automatic mission completion on all vehicles/people destruction with setting of opposite objective to failed
    
    * Victory by losses prefab & script
    
    * Added mission time component
    
    * Remove itself from callqueue after triggering objectives
    
    * Fix
    
    * Fix bug in losses script, add silenced spn for USSR

---
+++ Tushino_VanillaScenarios/Scripts/Game/Trigger/PS_PolyZoneObjectiveTriggerSave.c
@@ -0,0 +1,21 @@
+class PS_PolyZoneObjectiveTriggerSaveClass : PS_PolyZoneObjectiveTriggerDestroyClass
+{
+
+}
+
+/**
+    Use it to fail objective when all entities in trigger are destroyed and set it completed otherwise
+ */
+class PS_PolyZoneObjectiveTriggerSave : PS_PolyZoneObjectiveTriggerDestroy
+{
+	override void UpdateObjectives()
+	{
+		if (m_bAfterGame)
+			return;
+		
+		foreach (PS_Objective objective : m_aObjectives)
+		{
+			objective.SetCompleted(m_fDestroyedPercent < 1.0);
+		}
+	}
+}
