5ca1cb61b778186c46dc81e486f37fc3268dd47a
Boryan <tweedy.harfunkel@gmail.com>
2025-05-10 02:51:28 +0300

    Replication fixes

--- Tushino_VanillaScenarios/Scripts/Game/TSG_Objective.c
+++ Tushino_VanillaScenarios/Scripts/Game/TSG_Objective.c
@@ -6,11 +6,11 @@ class TSG_ObjectiveClass : PS_ObjectiveClass
 /**
 Just like regular objective but can be abandoned or assigned to faction
 */
-class TSG_Objective : PS_Objective
+class TSG_Objective: PS_Objective
 {
 	void Abandon()
 	{
-		SetFactionKey("");
+		SetFactionKey((FactionKey)"");
 	}
 
 	void SetFactionKey(FactionKey factionKey)
@@ -23,5 +23,25 @@ class TSG_Objective : PS_Objective
 	protected void RPC_SetFactionKey(FactionKey factionKey)
 	{
 		m_sFactionKey = factionKey;
+	}
+
+	// JIP Replication
+	override bool RplSave(ScriptBitWriter writer)
+	{
+		writer.WriteString(m_sFactionKey);
+		super.RplSave(writer);		
+		return true;
+	}
+
+	override bool RplLoad(ScriptBitReader reader)
+	{
+		reader.ReadString(m_sFactionKey);
+		super.RplLoad(reader);	
+		return true;
+	}
+	
+	override RplId GetRplId()
+	{
+		return m_RplComponent.ChildId(this);
 	}
 }
