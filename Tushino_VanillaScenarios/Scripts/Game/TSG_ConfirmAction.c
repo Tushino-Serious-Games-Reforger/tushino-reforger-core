226d2f53bbfeadc2c680972da59bd797b639f941
p1vasik228 <mopsjordon@gmail.com>
2025-07-30 04:00:10 +0300

    Новая миссия

---
+++ Tushino_VanillaScenarios/Scripts/Game/TSG_ConfirmAction.c
@@ -0,0 +1,44 @@
+class TSG_ConfirmAction : TSG_DeathConfirm
+{
+	[Attribute(defvalue: "USSR", desc: "Which faction can see action")]
+	FactionKey m_vFactionKey;
+	[Attribute(desc: "Should become unavailable after use?", defvalue: "true")]
+	bool m_vActionEnd
+	//------------------------------------------------------------------------------------------------
+	override bool CanBeShownScript(IEntity user)
+	{
+		return CanBePerformedScript(user);
+	}
+	
+	//------------------------------------------------------------------------------------------------
+	override bool CanBePerformedScript(IEntity user)
+	{
+		// Disallow action when wrong faction
+		if (!m_vFactionKey)
+			return false;
+		
+		FactionAffiliationComponent factionAffiliationComp = FactionAffiliationComponent.Cast(user.FindComponent(FactionAffiliationComponent));
+		if (!factionAffiliationComp)
+				return false;
+		
+		FactionManager factionManager = GetGame().GetFactionManager();
+		if (!factionManager)
+			return false;
+		
+		SCR_Faction faction = SCR_Faction.Cast(factionAffiliationComp.GetAffiliatedFaction());
+		if (faction != factionManager.GetFactionByKey(m_vFactionKey)) //check faction via factions
+			return false;
+		
+		if (m_vActionEnd == true)
+		{
+			foreach(string ObjectiveName : GetDoneObjectives())
+			{
+				PS_Objective Objective = PS_Objective.Cast(GetGame().GetWorld().FindEntityByName(ObjectiveName));
+				if (Objective.GetCompleted() == true)
+					return false;
+			}
+		}
+		
+		return true;
+	}
+}
